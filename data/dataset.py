9b100e2f3a34ecb1a25c6ad026841986064a44a8
Centauria <centauriavc@gmail.com>
2021-11-22 17:27:52 +0800

    add 2 datasets

--- data/dataset.py
+++ data/dataset.py
@@ -15,10 +15,10 @@ class SynthSet(Dataset):
         self.info_path = os.path.join(dataset_folder, 'info.csv')
         self.info = pd.read_csv(self.info_path)
         self.wav_dir = os.path.join(dataset_folder, 'wav')
-    
+
     def __len__(self):
         return len(self.info)
-    
+
     def __getitem__(self, item):
         values = self.info.loc[item]
         wav_path = os.path.join(self.wav_dir, values['wav_path'])
@@ -33,7 +33,7 @@ class SynthSetLPS(SynthSet):
         super(SynthSetLPS, self).__init__(dataset_folder)
         self.spec = Spectrogram(512, hop_length=256)
         self.a_d = AmplitudeToDB()
-    
+
     def __getitem__(self, item):
         x, y = super(SynthSetLPS, self).__getitem__(item)
         x = self.spec(x.mean(dim=0, keepdim=True))
@@ -49,12 +49,12 @@ class SynthSetHybrid(SynthSetLPS):
         return (x, s[:, :, :64].transpose(1, 2)), y
 
 
-class SynthSetWithClassify(SynthSet):
+class SynthSetClassify(SynthSet):
     def __init__(self, dataset_folder):
-        super(SynthSetWithClassify, self).__init__(dataset_folder)
+        super(SynthSetClassify, self).__init__(dataset_folder)
         self.spec = Spectrogram(512, hop_length=256)
         self.a_d = AmplitudeToDB()
-    
+
     def __getitem__(self, item):
         values = self.info.loc[item]
         wav_path = os.path.join(self.wav_dir, values['wav_path'])
@@ -62,7 +62,7 @@ class SynthSetWithClassify(SynthSet):
         assert sr == self.sr
         x = self.spec(x.mean(dim=0, keepdim=True))
         x = self.a_d(x)
-        x = x[:, :, :64].transpose(1, 2)
+        x = x.transpose(1, 2)
         y_args = np.hstack((
             values.values[:5].astype(np.float32),
             values.values[6:11].astype(np.float32)
@@ -73,3 +73,30 @@ class SynthSetWithClassify(SynthSet):
             'y_kind': y_kind
         }
         return x, y
+
+
+class SynthSetMiniClassify(SynthSetClassify):
+    def __getitem__(self, item):
+        x, y = super(SynthSetMiniClassify, self).__getitem__(item)
+        return x[:, :64, :], y
+
+
+class SynthSetHybridClassify(SynthSetClassify):
+    def __getitem__(self, item):
+        values = self.info.loc[item]
+        wav_path = os.path.join(self.wav_dir, values['wav_path'])
+        x, sr = torchaudio.load(wav_path)
+        assert sr == self.sr
+        s = self.spec(x.mean(dim=0, keepdim=True))
+        s = self.a_d(s)
+        s = s.transpose(1, 2)
+        y_args = np.hstack((
+            values.values[:5].astype(np.float32),
+            values.values[6:11].astype(np.float32)
+        ))
+        y_kind = torch.tensor(values.values[5] - 1, dtype=torch.long)
+        y = {
+            'y_args': y_args,
+            'y_kind': y_kind
+        }
+        return (x, s), y
