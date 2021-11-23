ab54cc0cb2991b22c475e4b9182337e4ebd8f594
Centauria <centauriavc@gmail.com>
2021-11-23 23:24:39 +0800

    correct E2EPretrained

--- models/E2E_ConvPretrained.py
+++ models/E2E_ConvPretrained.py
@@ -6,8 +6,8 @@ from models.ConvE2E import ConvE2E
 
 def get_model(conf: dict):
     model = ConvE2E()
-    checkpoint = torch.load(conf['pretrained_model']['conv_2d'])
-    model.conv_2d.load_state_dict(checkpoint['model'])
+    c = torch.load(conf['pretrained_model']['conv_2d'])
+    model.conv_2d.load_state_dict(c)
     for param in model.conv_2d.parameters():
         param.requires_grad = False
     return model
