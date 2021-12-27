625bd9dd7dfc3cd7018118a5aa07fba94566f983
Centauria <centauriavc@gmail.com>
2021-12-27 21:47:59 +0800

    handle tuple input

--- predict.py
+++ predict.py
@@ -17,22 +17,22 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', type=str, required=True)
     parser.add_argument('files', type=str, nargs='+')
     args = parser.parse_args()
-    
+
     if args.config is not None:
         conf = config.Config(args.config, **vars(args))
     else:
         conf = config.Config(**vars(args))
     print(conf)
-    
+
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    
+
     saved = torch.load(args.model_path)
     model = config.get_model(conf).to(device)
     model.load_state_dict(saved['model'])
     model.eval()
     synth_set = config.get_dataset(conf)(args.dataset_dir)
     keys = synth_set.info.keys()
-    
+
     with open(args.output, 'w') as f:
         writer = csv.DictWriter(f, keys)
         writer.writeheader()
@@ -48,7 +48,11 @@ if __name__ == '__main__':
                     file = os.path.join(file_or_folder, file)
                     print(f'Processing {file}')
                     x = synth_set.get(file)
-                    y = model(x.unsqueeze(0).to(device))
+                    if isinstance(x, tuple):
+                        x = list(map(lambda c: c.unsqueeze(0).to(device), x))
+                    else:
+                        x = x.unsqueeze(0).to(device)
+                    y = model(x)
                     r = synth_set.retrieve(y)
                     r['wav_path'] = file
             else:
